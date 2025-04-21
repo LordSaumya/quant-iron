7f2c18ab4981ae30dbc6a71ddc04f417fbd97235
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2025-04-21 23:20:56 +0200

    Adds common errors

---
+++ src/errors.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Invalid number of qubits: {0}")]
+    InvalidNumberOfQubits(usize),
+    #[error("Invalid qubit index: {0} for {1} qubits")]
+    InvalidQubitIndex(usize, usize),
+    #[error("State vector is not normalised")]
+    StateVectorNotNormalised,
+    #[error("An unknown error occurred")]
+    UnknownError,
+}
