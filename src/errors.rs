05ab8134803bb113ee27d53db00db19ad44feefe
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2025-04-29 21:05:05 +0200

    Add error for overlapping control and target qubits

--- src/errors.rs
+++ src/errors.rs
@@ -8,6 +8,16 @@ pub enum Error {
     #[error("Invalid number of measurements: {0}")]
     InvalidNumberOfMeasurements(usize),
 
+    /// The control qubits and target qubit indices are overlapping
+    /// 
+    /// # Arguments:
+    /// 
+    /// * `0` - The control qubit index
+    /// 
+    /// * `1` - The target qubit index
+    #[error("Control qubit index {0} overlaps with target qubit index {1}")]
+    OverlappingControlAndTargetQubits(usize, usize),
+
     /// There is an invalid number of qubits
     /// 
     /// # Arguments:
