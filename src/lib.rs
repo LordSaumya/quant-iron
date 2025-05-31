441a6a4a09a026749119832ea97c0108cc91b7ce
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2025-05-31 16:01:43 +0200

    Update re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,20 @@ pub mod components;
 pub mod errors;
 pub mod subroutine;
 pub mod models;
+pub mod algorithms;
 
 // Re-export important types for easier imports
-pub use crate::components::gate::Gate;
 pub use crate::components::measurement::{MeasurementBasis, MeasurementResult};
 pub use crate::components::operator::{
     CNOT, Hadamard, Identity, Operator, Pauli, PhaseS, PhaseSdag, PhaseShift, PhaseT, PhaseTdag,
-    RotateX, RotateY, RotateZ, SWAP, Toffoli,
+    RotateX, RotateY, RotateZ, SWAP, Toffoli, Unitary2
 };
 pub use crate::components::state::{ChainableState, State};
+pub use crate::components::pauli_string::{PauliString, SumOp};
+pub use crate::circuit::{Circuit, CircuitBuilder};
+pub use crate::subroutine::Subroutine;
+pub use crate::models::{heisenberg, ising};
+pub use crate::algorithms::time_evolution;
 
 #[cfg(test)]
 pub mod tests;
