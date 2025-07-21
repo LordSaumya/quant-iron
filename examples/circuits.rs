c41fb9293a06c8e009f3731ef1ac895704c00f22
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2025-07-21 23:15:02 +0800

    Fix spacing

--- examples/circuits.rs
+++ examples/circuits.rs
@@ -1,6 +1,6 @@
 use quant_iron::{CircuitBuilder, MeasurementBasis, State, Subroutine};
 
-/// Build a quantum circuit with a QFT subroutine and execute it on a state:**
+/// Build a quantum circuit with a QFT subroutine and execute it on a state
 
 fn main() {
     // Build a circuit with 3 qubits
@@ -8,15 +8,15 @@ fn main() {
         .h_gate(0)                                                  // Add a Hadamard gate on qubit 0
         .cnot_gate(0, 1)                                            // Add a CNOT gate with control=0 and target=1
         .x_gates(vec![1, 2])                                        // Add Pauli-X gates on qubits 1 and 2
-        .add_subroutine(Subroutine::qft(vec![1, 2], 3))             // Add a QFT subroutine on qubits 1 and 2 for the 3 qubit system
+        .add_subroutine(Subroutine::qft(vec![1, 2], 3))             // Add a QFT subroutine on qubits 1 and 2 for the 3-qubit system
         .measure_gate(MeasurementBasis::Computational, vec![0, 1])  // Measure qubits 0 and 1
         .build()
-        .expect("Could not build Circuit");                                                   // Build the circuit
+        .expect("Could not build Circuit");                         // Build the circuit
 
     let state = State::new_plus(3).expect("Could not create state");
     println!("Initial state: {:?}", state);            // Print the new state after execution
 
     let new_state = circuit.execute(&state)
-        .expect("Could not execute circuit");        // Execute the circuit on the |++> state
+        .expect("Could not execute circuit");          // Execute the circuit on the |++> state
     println!("New state: {:?}", new_state);            // Print the new state after execution
 }
