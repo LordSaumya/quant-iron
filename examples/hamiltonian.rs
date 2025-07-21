e3b166672efe626ae2699552e37d5f54225e7c50
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2025-07-21 23:15:44 +0800

    Fix spacing

--- examples/hamiltonian.rs
+++ examples/hamiltonian.rs
@@ -1,9 +1,9 @@
 use quant_iron::{Pauli, PauliString, State, SumOp};
 
-/// Define a Hamiltonian and compute its expectation value:**
+/// Define a Hamiltonian and compute its expectation value
 fn main() {
     // Define a Hamiltonian for a 2-qubit system
-    let hamiltonian = SumOp::new(Vec::default())                                                  // 2 X_0 + Y_1 + 0.5 Z_0 X_1
+    let hamiltonian = SumOp::new(Vec::default())                                             // 2 X_0 + Y_1 + 0.5 Z_0 X_1
         .with_term(PauliString::new(2.0.into()).with_op(0, Pauli::X))                        // 2X_0
         .with_term(PauliString::new(1.0.into()).with_op(1, Pauli::Y))                        // Y_1
         .with_term(PauliString::new(0.5.into()).with_op(0, Pauli::Z).with_op(1, Pauli::X));  // 0.5Z_0 X_1
