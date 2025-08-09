4890909bd23f4509c971961d742097612959239f
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2025-08-09 16:09:43 +0800

    Add parameters for parametric gates

---
+++ src/components/parametric/mod.rs
@@ -0,0 +1,7 @@
+//! # Parametric Components
+//! 
+//! This module provides support for parametric quantum operations, allowing for the definition and manipulation of quantum gates and operators with parameters.
+//! It includes structures for defining parameters, as well as parametric gates that can be used in quantum circuits.
+pub mod parameter;
+pub mod parametric_gate;
+
---
+++ src/components/parametric/parameter.rs
@@ -0,0 +1,57 @@
+use std::sync::{Arc, Mutex};
+
+/// A struct representing a set of mutable parameters for a parametrised gate.
+/// Cloning this struct is cheap and allows multiple gates to share the same underlying parameters.
+///
+/// # Fields
+/// - `values`: An `Arc<Mutex<[f64; N]>>` that holds the parameter values.
+///
+/// # Generic Parameters
+/// - `N`: The number of values in the parameter array.
+#[derive(Debug, Clone)]
+pub struct Parameter<const N: usize> {
+    /// The values of the parameters
+    values: Arc<Mutex<[f64; N]>>,
+}
+
+impl<const N: usize> Parameter<N> {
+    /// Creates a new `Parameter` instance with the given initial values.
+    ///
+    /// # Arguments
+    ///
+    /// * `initial_values` - An array of initial parameter values.
+    ///
+    /// # Returns
+    ///
+    /// A new `Parameter` instance.
+    pub fn new(initial_values: [f64; N]) -> Self {
+        Self {
+            values: Arc::new(Mutex::new(initial_values)),
+        }
+    }
+
+    /// Gets the current values of the parameters.
+    /// This will lock the underlying mutex.
+    ///
+    /// # Returns
+    ///
+    /// The current parameter values.
+    ///
+    /// If the mutex is poisoned, the function will return the current values.
+    pub fn get(&self) -> [f64; N] {
+        match self.values.lock() {
+            Ok(guard) => *guard,
+            Err(poisoned) => *poisoned.into_inner(),
+        }
+    }
+
+    /// Sets the values of the parameters.
+    /// This will lock the underlying mutex.
+    pub fn set(&self, new_values: [f64; N]) {
+        let mut guard = match self.values.lock() {
+            Ok(guard) => guard,
+            Err(poisoned) => poisoned.into_inner(),
+        };
+        *guard = new_values;
+    }
+}
